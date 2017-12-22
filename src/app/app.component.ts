c73702c4f036234c3fe285ab1803d66eea6d952c
Rija RAMAMPIANDRA <rijaramampiandra@gmail.com>
2017-12-22 11:53:40 +0300

    <dev> Tour of heroes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Tour of Heroes';
 }
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -14,3 +14,10 @@ export class MessagesComponent implements OnInit {
   }
 
 }
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
